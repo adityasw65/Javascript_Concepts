b52886364cfeeb3ff658c8893ee21723bd1deb26
Aditya Satyawan Waghmare <142006839+adityasw65@users.noreply.github.com>
2024-12-04 20:10:11 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,13 @@
+// comments 
+// first way is for single line comment 
+/* 
+second way 
+is for 
+multiline comment 
+*/
+
+// second way to connect js with html 
+alert("Hello LinkedIn People");
+
+
+
